cf5a56b5e7b966992e8d142ee89ecd73db6bb014
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2024-11-27 11:05:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ navLinks.forEach(link => {
     targetSection.style.display = 'block';
   });
 });
+
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbarCollapse.classList.remove('show');
+  });
+});
+

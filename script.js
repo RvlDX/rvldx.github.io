d0680f6dec451a9927c7a3c59e5ee39fd561aab7
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2024-11-27 11:44:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-const navbarCollapse = document.querySelector('.navbar-collapse');
+const navLinks = document.querySelectorAll('.nav-link');
 const sections = document.querySelectorAll('section');
 
 sections.forEach(section => {
@@ -21,10 +20,3 @@ navLinks.forEach(link => {
     targetSection.style.display = 'block';
   });
 });
-
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navbarCollapse.classList.remove('show');
-  });
-});
-

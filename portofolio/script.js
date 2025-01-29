8e96e3d0ed43d643d65083c759aa76e0f4fd55a6
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2025-01-29 20:36:40 +0700

    Update portofolio

---
+++ portofolio/script.js
@@ -0,0 +1,37 @@
+// script.js
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const sections = document.querySelectorAll('.section');
+const navLinksA = document.querySelectorAll('.nav-links a');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+navLinksA.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default link behavior
+
+        const targetSectionId = link.getAttribute('data-section');
+
+        // Hide all sections
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+
+        // Show the target section
+        const targetSection = document.getElementById(targetSectionId);
+        if (targetSection) {
+            targetSection.classList.add('active');
+        }
+
+        // Close the menu on smaller screens after clicking
+        if (window.innerWidth <= 768) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
+
+// Show the home section by default on load
+document.getElementById('home').classList.add('active');
+
